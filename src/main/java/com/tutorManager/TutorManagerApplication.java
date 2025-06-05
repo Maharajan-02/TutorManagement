43cd8788141c69d87c32f189c4ee79bb023cc857
Maharajan-02 <rajascientist98@gmail.com>
2025-06-05 21:07:39 +0530

    Initial commit

---
+++ src/main/java/com/tutorManager/TutorManagerApplication.java
@@ -0,0 +1,13 @@
+package com.tutorManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorManagerApplication.class, args);
+	}
+
+}
